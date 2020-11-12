7004fbab571ed90fad24503c2716a26f648a897f
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-11-12 11:03:03 +0100

    Add files via upload

---
+++ tomcMatijaPonavljanjeZd3/tomcMatijaPonavljanjeZd3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaPonavljanjeZd3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] niz = new string[3];
+
+            Console.WriteLine("Upišite prvi string:");
+            niz[0] = Console.ReadLine();
+
+            Console.WriteLine("Upišite drugi string:");
+            niz[1] = Console.ReadLine();
+
+            Console.WriteLine("Upišite treci string:");
+            niz[2] = Console.ReadLine();
+
+            Array.Sort(niz);
+            Array.Reverse(niz);
+
+            Console.WriteLine("Sortirani stringovi:");
+            for(int i=0; i<3; i++)
+            {
+                Console.WriteLine(niz[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
